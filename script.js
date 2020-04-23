42a460f6bc7b4cd0d8db95e7dfc49fb40e337b03
Inez Escandon <iescan4@gmail.com>
2020-04-23 17:16:38 -0500

    added conditional statements in passwordQuestions function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Declare a variable and assign it a value
 var generateBtn = document.querySelector("#generate");
 var passwordCharacters = [];
 var uppercaseLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -6,59 +7,72 @@ var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharactersArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
 
 
+//  Ask user for password criteria, store in variables, create an object, return object
 function passwordQuestions() {
+  resetApp();
   var passwordLength = prompt('How long would you like your password to be? Choose a number between 8 - 128.');
+
+  if (passwordLength < 8 || passwordLength > 128) {
+      alert('Your selected password length has to be at least 8 characters and no more than 128.');
+      passwordQuestions();
+  }
+  
   var uppercaseLetters = confirm('Would you like uppercase letters?');
   var lowercaseLetters = confirm('Would you like lowercase letters?');
   var numbers = confirm ('Would you like numbers?');
   var specialCharacters = confirm ('Would you like special characters?');
+
+  if (!uppercaseLetters && !lowercaseLetters && !numbers && !specialCharacters) {
+      alert('Please select at least one character type to include in your password.');
+      passwordQuestions();
+  }
+
   var options = {
      passwordLength: passwordLength,
      uppercaseLetters: uppercaseLetters,
      lowercaseLetters: lowercaseLetters,
      numbers: numbers,
      specialCharacters: specialCharacters}
+
   return options;
 }
 
+
+// if statements to add chosen arrays to passwordCharacters array, for loop to choose random letter for user preferences length
 function generatePassword() {
   var options = passwordQuestions();
-  // console.log(options);
-  //alert(options)
+
   if (options.uppercaseLetters) {
-    // alert("You are using uppercase letters.")
     passwordCharacters = passwordCharacters.concat(uppercaseLettersArray);
   }
+
   if (options.lowercaseLetters) {
     passwordCharacters = passwordCharacters.concat(lowercaseLettersArray);
   }
+
   if (options.numbers) {
     passwordCharacters = passwordCharacters.concat(numbersArray);
   }
+
   if (options.specialCharacters) {
     passwordCharacters = passwordCharacters.concat(specialCharactersArray);
   }
 
+  var password = "";
+
   for (var i = 0; i < options.passwordLength; i++) {
-
       var randomCharacter = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-      var password = password + randomCharacter;
-
-
-
-
-    // write the logic on each loop to pick one random char from passwordCharacters arr.
-    // you need to push this random char into (something)
+      password = password + randomCharacter;
   }
   
   return password;
-  //return (something)
-
-  //reset stats?
 }
 
 
-// Assignment Code
+// Function to reset passwordCharacters array
+function resetApp() {
+    passwordCharacters = [];
+}
 
 
 // Write password to the #password input
@@ -69,7 +83,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
 // Add event listener to generate button
-//document.querySelector("#generate").addEventListener("click", writePassword);
 generateBtn.addEventListener("click", writePassword);
 
